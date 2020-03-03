6c24524c1be6dd1ae666fdb4cdf7e7f04ed629e8
bolajikassim <kassim.bolaji@yahoo.com>
2020-03-03 12:04:12 +0100

    removed mvvm and mvp patterns

--- app/src/main/java/com/bolaware/architectureexample/view/MainActivity.kt
+++ app/src/main/java/com/bolaware/architectureexample/view/MainActivity.kt
@@ -8,31 +8,19 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bolaware.architectureexample.MainContract
 import com.bolaware.architectureexample.R
-import com.bolaware.architectureexample.data.Movie
-import com.bolaware.architectureexample.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val INITIAL_SEARCH_TERM = "Game"
 
-class MainActivity : AppCompatActivity(), MainContract.View {
-
-    val viewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setUpView()
-
-        setUpObserver()
 
         initAMovieSearch()
     }
@@ -44,6 +32,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
         moviesRV.layoutManager =
             CenterZoomLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
         moviesRV.adapter = MoviesAdapter(mutableListOf())
 
         searchET.setOnEditorActionListener( object : TextView.OnEditorActionListener {
@@ -51,7 +40,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH
                     || event?.action == KeyEvent.ACTION_DOWN
                     && event.keyCode == KeyEvent.KEYCODE_ENTER) {
-                    viewModel.searchMovies(searchET.text.toString())
+
                     hideKeyboard()
                     return true
                 }
@@ -60,40 +49,9 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         })
     }
 
-    private fun setUpObserver(){
-        viewModel.moviesLd.observe(this, Observer {
-            it?.let {
-                (moviesRV.adapter as MoviesAdapter?)?.updateMovies(it)
-            }
-        })
-
-        viewModel.errorLd.observe(this, Observer {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        })
-
-        viewModel.progressIndicatorLd.observe(this, Observer {
-            progressBar.visibility = if(it == true){
-                View.VISIBLE
-            } else{
-                View.GONE
-            }
-        })
-    }
 
     private fun initAMovieSearch(){
-        viewModel.searchMovies(INITIAL_SEARCH_TERM)
-    }
-
-    override fun showProgressIndicator(show: Boolean) {
-
-    }
-
-    override fun displayMovies(movies: List<Movie>) {
-
-    }
-
-    override fun showToast(message: String) {
-
+        //viewModel.searchMovies(INITIAL_SEARCH_TERM)
     }
 }
 
